b59ef0a18029f1012f7bfda0fe91567a77720185
Srimathir <srimathiru12@gmail.com>
2025-02-24 15:42:23 +0530

    First commit

---
+++ src/test/java/DriverInititation/chromeDriver.java
@@ -0,0 +1,14 @@
+package DriverInititation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class chromeDriver {
+    public static void main(String[] args) {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.google.co.in/");
+        driver.close();
+
+    }
+}
